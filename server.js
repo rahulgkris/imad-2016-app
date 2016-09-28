b85cc1317570db27a49fb9e90b7c0c57bbd18499
rahulgkris <rahulgkrishnan@gmail.com>
2016-09-28 23:03:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('zarticle-one.html');
+  res.send('sssindex');
 });
 
 app.get('/article-two', function (req, res) {
