0c71a22bba4fa424cfa1191691ec2e2fa654259f
rahulgkris <rahulgkrishnan@gmail.com>
2016-10-02 21:59:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,6 +9,9 @@ var marginLeft = 0;
 function moveRight(){
     marginLeft =marginLeft + 10;
     img.style.marginLeft=marginLeft + 'px';
+    if (marginLeft > 50) then
+    marginLeft = 0;
+    
 }
 img.onclick = function(){
     img.style.marginRight= '100px';
