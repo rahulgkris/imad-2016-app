5c1a598eb33e2661f35d201dd7e4e6681b9e7d1d
rahulgkris <rahulgkrishnan@gmail.com>
2016-10-02 21:04:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,4 @@ var element = document.getElementById('maintext');
 
 element.innerHTML = 'Hopesprings';
 
-var img = document.getElementById('madi');
-img.onclick = function(){
-    img.style.marginRight= '100px';
-} 
+
