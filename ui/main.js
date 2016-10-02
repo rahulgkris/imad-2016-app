7c223a0075b37054712dbdf72b2dd60549c47c93
rahulgkris <rahulgkrishnan@gmail.com>
2016-10-02 20:48:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+
+var element = document.getElementById('maintext');
+element.innerHTML = 'Hopespeings';
+
+var madi = document.getElementById('madi');
+madi.onclick = function(){
+    
+} 
