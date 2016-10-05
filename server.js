d2d9564c1ec5445edc70a448235ca1659c1b747f
rahulgkris <rahulgkrishnan@gmail.com>
2016-10-05 09:04:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,16 +97,16 @@ app.get('/', function (req, res) {
   var articleName = req.params.articleName;    
   res.send(createTemplate(articles[articleName]));
 });**/
+var counter = 0;
+app.get('/counter', function(req, res){
+   counter = counter + 1;
+   res.send(counter,toString());
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-var counter = 0;
-app.get('/counter', function(req, res){
-   counter = counter + 1;
-   res.send(counter, toString());
-});
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
